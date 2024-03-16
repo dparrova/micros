55c93afd393f3e0c08ca7b3223d7b3cdd1339336
Diego Chaparro <dfchaparrov@unal.edu.co>
2024-03-16 11:14:17 -0500

    Avance lab02

--- Laboratorio2.X/Laboratorio2.asm
+++ Laboratorio2.X/Laboratorio2.asm
@@ -1,19 +1,24 @@
 ;librerias
     include p18f4550.inc
-    ;Configuracion
+;Configuracion
    CONFIG FOSC = EC_EC
    CONFIG WDT = OFF ; oscilador externo, y WatchDog apagado
-  ;variables
-  
-  ;instrucciones
-Inicio
-  CODE
-  bsf TRISD,0 ;configura como entrada
-  bsf TRISD,1 ;configura como una entrada
-  bsf TRISD,2 ;
+   CONFIG LVP = OFF
+;variables
+  aux1 equ 0h
+;instrucciones
+Inicio 
+ code
+  bcf TRISD,0 ;configura como salida
+  bcf TRISD,1 ;configura como una salida
+  bcf TRISD,2 ;
   Ciclo
   bcf LATD,0 
   bsf LATD,0
+  bcf LATD,1 
+  bsf LATD,1
+  bcf LATD,2 
+  bsf LATD,2
 
 goto Ciclo   
 end
--- Laboratorio2.X/nbproject/Makefile-default.mk
+++ Laboratorio2.X/nbproject/Makefile-default.mk
@@ -84,7 +84,7 @@ endif
 	${MAKE}  -f nbproject/Makefile-default.mk dist/${CND_CONF}/${IMAGE_TYPE}/Laboratorio2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 
 MP_PROCESSOR_OPTION=18f4550
-MP_LINKER_DEBUG_OPTION= 
+MP_LINKER_DEBUG_OPTION=-r=ROM@0x7DC0:0x7FFF -r=RAM@GPR:0x3F4:0x3FF
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: assemble
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
@@ -92,7 +92,7 @@ ${OBJECTDIR}/Laboratorio2.o: Laboratorio2.asm  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/Laboratorio2.o.d 
 	@${RM} ${OBJECTDIR}/Laboratorio2.o 
-	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/Laboratorio2.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_SIMULATOR=1 -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/Laboratorio2.lst\" -e\"${OBJECTDIR}/Laboratorio2.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/Laboratorio2.o\" \"Laboratorio2.asm\" 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/Laboratorio2.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PICKIT2=1 -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/Laboratorio2.lst\" -e\"${OBJECTDIR}/Laboratorio2.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/Laboratorio2.o\" \"Laboratorio2.asm\" 
 	@${DEP_GEN} -d "${OBJECTDIR}/Laboratorio2.o"
 	@${FIXDEPS} "${OBJECTDIR}/Laboratorio2.o.d" $(SILENT) -rsi ${MP_AS_DIR} -c18 
 	
@@ -112,7 +112,7 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/Laboratorio2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w -x -u_DEBUG -z__ICD2RAM=1 -m"${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map"   -z__MPLAB_BUILD=1  -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_SIMULATOR=1 $(MP_LINKER_DEBUG_OPTION) -odist/${CND_CONF}/${IMAGE_TYPE}/Laboratorio2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w -x -u_DEBUG -z__ICD2RAM=1 -m"${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map"   -z__MPLAB_BUILD=1  -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_PICKIT2=1 $(MP_LINKER_DEBUG_OPTION) -odist/${CND_CONF}/${IMAGE_TYPE}/Laboratorio2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/Laboratorio2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
