8729a9df471de90de23f289b939d3bf87775db49
Diego Chaparro <dfchaparrov@unal.edu.co>
2024-03-16 23:28:05 -0500

    Codigo lab2 terminado

--- Laboratorio2.X/Laboratorio2.asm
+++ Laboratorio2.X/Laboratorio2.asm
@@ -3,22 +3,38 @@
 ;Configuracion
    CONFIG FOSC = EC_EC
    CONFIG WDT = OFF ; oscilador externo, y WatchDog apagado
-   CONFIG LVP = OFF
+   CONFIG LVP = OFF ; 
 ;variables
   aux1 equ 0h
 ;instrucciones
 Inicio 
  code
-  bcf TRISD,0 ;configura como salida
-  bcf TRISD,1 ;configura como una salida
+  bcf TRISD,0 ;configuracion como salidas
+  bcf TRISD,1 ;
   bcf TRISD,2 ;
   Ciclo
-  bcf LATD,0 
-  bsf LATD,0
-  bcf LATD,1 
-  bsf LATD,1
-  bcf LATD,2 
-  bsf LATD,2
-
+  ;bcf LATD,0 
+  ;bsf LATD,0
+  ;bcf LATD,1 
+  ;bsf LATD,1
+  ;bcf LATD,2 
+  ;bsf LATD,2
+  ;codigo de colores en logica inversa por ser una RGB de anodo común.
+  movlw b'00000011' ;Azul
+  movwf LATD 
+  movlw b'00000001' ;Cyan
+  movwf LATD
+  movlw b'00000101' ;Verde
+  movwf LATD
+  movlw b'0000100' ;Amarillo
+  movwf LATD
+  movlw b'00000000' ;Blanco
+  movwf LATD
+  movlw b'00000010' ;Magenta
+  movwf LATD
+  movlw b'00000110' ;Rojo
+  movwf LATD
+  movlw b'0000111' ;Negro
+  movwf LATD
 goto Ciclo   
 end
