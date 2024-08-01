35277438b8c70e71815e55d976a98ac8e92a9bad
Diego Chaparro <dfchaparrov@unal.edu.co>
2024-07-31 21:51:16 -0500

    simulacion con led RGB

--- Laboratorio03.X/Laboratorio03.c
+++ Laboratorio03.X/Laboratorio03.c
@@ -3,25 +3,38 @@
 #pragma config WDT = OFF
 #pragma config FOSC = INTOSC_EC
 #pragma config LVP = OFF
-void main(void) {
+#pragma config PBADEN = OFF
+
+void __interrupt() ISR(void);
+
+void main(void) { 
     TRISE=0;
     TRISD=0;
     TRISA=0;
     TRISB=1;
     LATD = 0b00000000; // DISPLAY  SEGMENTS
     LATE = 0b00000111; //LED RGB
+    INTEDG0=1;
+    INT0IF=0;
+    INT0IE=1;
+    GIE=1;
       while(1){
-      if(LATD==0b00001010){
+          LATD++;
+      if(LATD==0b00001010)
+      {
               LATD=0;
               LATE--;
+            
       }
       __delay_ms(1000);    
-     LATA1=!LATA1; 
+     LATA1=!LATA1; //FUNCIONANDO!
+     
     }
     return;
 }
 
 void __interrupt() ISR(void){
+    
  if(INT0IF==1){
      INT0IF=0;
      LATD++; 
