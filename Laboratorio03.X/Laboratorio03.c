01924b7f002e8ceacbc7c2c1fabbe35284770652
Diego Chaparro <dfchaparrov@unal.edu.co>
2024-08-21 11:32:45 -0500

    codigo agregado laboratorio03

--- Laboratorio03.X/Laboratorio03.c
+++ Laboratorio03.X/Laboratorio03.c
@@ -5,6 +5,12 @@
 #pragma config LVP = OFF
 #pragma config PBADEN = OFF
 
+
+int colorIndice = 0;
+int color[] = {2 , 6 , 4 , 5 , 1 , 0}; //magenta, azul, cyan, verde, amarillo y blanco
+int rojo = 3;
+int parar = 1; //logica negativa
+
 void __interrupt() ISR(void);
 
 void main(void) {
@@ -21,32 +27,45 @@ void main(void) {
     TMR0IE = 1;
     TMR0ON = 1; // fin TMR0
 
-    INTEDG0 = 1;
+    INTEDG0 = 1; //Inicio configuración pulsador
     INT0IF = 0;
-    INT0IE = 1;
+    INT0IE = 1; // fin pulsador
+    
+    INT1IF = 0; // Inicio configuración interruptor
+    INT1IE = 1; // fin interruptor
 
     GIE = 1;
-    while (1) {
-        // LATD++;
-        if (LATD == 0b00001010) {
+     while (1) {
+            
+            if (LATD == 0b00001010) {
             LATD = 0;
-            LATE--;
-        }
-        //  __delay_ms(1000);    
-
-
+            LATE=color[colorIndice%6];
+            colorIndice++;
+        }   
+            if(parar){
+            __delay_ms(1000);
+            LATD++;
+            }
     }
     return;
 }
 
 void __interrupt() ISR(void) {
     if (TMR0IF == 1) {
-        TMR0 = 3036;
+        TMR0 = 3036; //PRESET
         TMR0IF = 0;
-        LATA1 = !LATA1; //FUNCIONANDO!
+        LATA1 = !LATA1; //toggle!
     }
     if (INT0IF == 1) {
-        INT0IF = 0;
-        LATD++;
+       INT0IF = 0;
+       if(parar){
+       LATD = 0;
+       LATE = 0b00000111;
+       colorIndice = 0;}
+    }
+    if (INT1IF == 1) {
+       INT1IF = 0;
+       LATE = rojo;
+       parar = 0;
     }
 }
